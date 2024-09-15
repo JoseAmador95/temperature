d08d82c041876f2790cb51d8609be05426779e54
Jose Amador Demeneghi <20510893+JoseAmador95@users.noreply.github.com>
2024-09-15 23:17:59 +0100

    Switched to enum traits

--- src/main.rs
+++ src/main.rs
@@ -5,45 +5,60 @@ mod temperature;
 
 #[derive(Debug)]
 enum Unit {
-    Celcius(temperature::Temperature),
-    Farenheit(temperature::Temperature),
-    Kelvin(temperature::Temperature),
+    Celcius,
+    Farenheit,
+    Kelvin,
 }
 
-fn parse_unit(unit: &String, temp: temperature::Temperature) -> Result<Unit, String> {
-    match unit.as_str() {
-        cli::CELCIUS_SHORT_NAME => Ok(Unit::Celcius(temp)),
-        cli::FARENHEIT_SHORT_NAME => Ok(Unit::Farenheit(temp)),
-        cli::KELVIN_SHORT_NAME => Ok(Unit::Kelvin(temp)),
-        _ => Err(format!("Invalid unit: {unit}", unit = unit)),
+impl Unit {
+    fn from_str(s: &str) -> Result<Self, String> {
+        match s {
+            cli::CELCIUS_SHORT_NAME => Ok(Unit::Celcius),
+            cli::FARENHEIT_SHORT_NAME => Ok(Unit::Farenheit),
+            cli::KELVIN_SHORT_NAME => Ok(Unit::Kelvin),
+            _ => Err(format!("Invalid unit: {unit}", unit = s)),
+        }
+    }
+
+    fn to_str_unit(&self) -> &'static str {
+        match self {
+            Unit::Celcius => "°C",
+            Unit::Farenheit => "°F",
+            Unit::Kelvin => "K",
+        }
     }
 }
 
-fn convert_temp(input: Unit, output: Unit) -> Unit {
-    match (input, output) {
-        (Unit::Celcius(c), Unit::Celcius(_)) => Unit::Celcius(c),
-        (Unit::Celcius(c), Unit::Farenheit(_)) => {
-            Unit::Farenheit(temperature::celcius_to_fahrenheit(c))
-        }
-        (Unit::Celcius(c), Unit::Kelvin(_)) => Unit::Kelvin(temperature::celcius_to_kelvin(c)),
-        (Unit::Farenheit(f), Unit::Farenheit(_)) => Unit::Farenheit(f),
-        (Unit::Farenheit(f), Unit::Celcius(_)) => {
-            Unit::Celcius(temperature::fahrenheit_to_celcius(f))
-        }
-        (Unit::Farenheit(f), Unit::Kelvin(_)) => Unit::Kelvin(temperature::fahrenheit_to_kelvin(f)),
-        (Unit::Kelvin(k), Unit::Kelvin(_)) => Unit::Kelvin(k),
-        (Unit::Kelvin(k), Unit::Celcius(_)) => Unit::Celcius(temperature::kelvin_to_celcius(k)),
-        (Unit::Kelvin(k), Unit::Farenheit(_)) => {
-            Unit::Farenheit(temperature::kelvin_to_fahrenheit(k))
-        }
+fn convert_temp(
+    temp: temperature::Temperature,
+    input_unit: &Unit,
+    output_unit: &Unit,
+) -> temperature::Temperature {
+    match (input_unit, output_unit) {
+        (Unit::Celcius, Unit::Celcius) => temp,
+        (Unit::Celcius, Unit::Farenheit) => temperature::celcius_to_fahrenheit(temp),
+        (Unit::Celcius, Unit::Kelvin) => temperature::celcius_to_kelvin(temp),
+        (Unit::Farenheit, Unit::Farenheit) => temp,
+        (Unit::Farenheit, Unit::Celcius) => temperature::fahrenheit_to_celcius(temp),
+        (Unit::Farenheit, Unit::Kelvin) => temperature::fahrenheit_to_kelvin(temp),
+        (Unit::Kelvin, Unit::Kelvin) => temp,
+        (Unit::Kelvin, Unit::Celcius) => temperature::kelvin_to_celcius(temp),
+        (Unit::Kelvin, Unit::Farenheit) => temperature::kelvin_to_fahrenheit(temp),
     }
 }
 
 fn main() {
     let args = cli::Args::parse_from(std::env::args());
-    let input = parse_unit(&args.input_unit, args.temperature).unwrap();
-    let output_unit = parse_unit(&args.output_unit, 0.0).unwrap();
-    println!("input={input:?}, output_unit={output_unit:?}");
-    let output = convert_temp(input, output_unit);
-    println!("output={output:?}");
+    let input_unit = Unit::from_str(&args.input_unit).unwrap();
+    let output_unit = Unit::from_str(&args.output_unit).unwrap();
+    println!(
+        "input={temp:?}, input_unit={input_unit:?}, output_unit={output_unit:?}",
+        temp = args.temperature
+    );
+    let output = convert_temp(args.temperature, &input_unit, &output_unit);
+    println!(
+        "output={output:?}{unit}",
+        output = output,
+        unit = output_unit.to_str_unit()
+    );
 }
